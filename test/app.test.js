4e265819a59709dd5655a238591f05bcfa054b2f
davidlruizc <druiz@dbsolution.com.co>
2020-01-29 09:41:03 -0500

    adding folder for test

---
+++ test/app.test.js
@@ -0,0 +1,11 @@
+const app = require('../app')
+const supertest = require('supertest')
+const request = supertest(app)
+
+it('Test Get method endpoint', async (done) => {
+  const response = await request.get('/')
+
+  expect(response.status).toBe(200)
+  expect(response.body.res).toBe('Hello World!')
+  done()
+})
